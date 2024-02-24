36235d665eadc31b8ff70017bb10cc89947bbae1
botball345 <102452108+botball345@users.noreply.github.com>
2024-02-23 16:49:40 -0800

    Add files via upload
    2/23/24
    Space drawer mission

---
+++ main (1).c
@@ -0,0 +1,83 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    printf("Hello World\n");
+   
+    enable_servos();
+    
+    set_servo_position(0, 1265);
+    
+    set_servo_position(3, 1000);
+    
+    //forward
+    
+    motor(0,100);
+    
+    motor(3,100);
+    
+    msleep(2700);
+    
+    
+    //turn
+    
+    motor(0,0);
+    
+    motor(3,100);
+    
+    msleep(800);
+    
+    
+    //forward
+    
+    motor(0,100);
+    
+    motor(3,100);
+    
+    msleep(920);
+    
+    
+    //close claw
+    
+    set_servo_position(3, 2047);
+    
+    msleep(1000);
+    
+    
+    //stop
+    
+    motor(0, 0);
+    
+    motor(3, 0);
+    
+    msleep(1000);
+    
+    
+    //backwards
+    
+    motor(0, -100);
+   
+    motor(3, -100);
+    
+    msleep(150);
+    
+    
+    //close claw
+   
+    set_servo_position(3,2047);
+    
+    msleep(1000);
+    
+    
+    //raise arn
+    
+    set_servo_position(0,1085);
+    
+    msleep(1000);
+ 
+    
+    
+    
+    
+    return 0;
+}
