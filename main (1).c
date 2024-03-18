c1e4fecaafef1461072f14fa078fd361e2330fb0
botball345 <102452108+botball345@users.noreply.github.com>
2024-03-18 16:53:30 -0700

    Add files via upload
    3-18-24 code

--- main (1).c
+++ main (1).c
@@ -4,80 +4,216 @@ int main()
 {
     printf("Hello World\n");
    
+    
+    int left = 3;
+    
+    int right = 0;
+    
+    int arm = 1;
+    
+    int claw = 0;
+    
     enable_servos();
     
-    set_servo_position(0, 1265);
+    set_servo_position(arm, 223);
     
-    set_servo_position(3, 1000);
+    msleep(1000);
+    
+    set_servo_position(arm, 1435);
+    
+    set_servo_position(claw, 1560);
+    
+    
     
     //forward
     
-    motor(0,100);
+    motor(left,100);
     
-    motor(3,100);
+    motor(right,100);
     
-    msleep(2700);
+    msleep(3000);
     
     
     //turn
     
-    motor(0,0);
+    motor(left,0);
     
-    motor(3,100);
+    motor(right,110);
     
-    msleep(800);
+    msleep(860);
     
     
     //forward
     
-    motor(0,100);
+    motor(left,100);
     
-    motor(3,100);
+    motor(right,100);
     
     msleep(920);
     
     
     //close claw
     
-    set_servo_position(3, 2047);
+    set_servo_position(claw, 2047);
     
     msleep(1000);
     
     
     //stop
     
-    motor(0, 0);
+    motor(left, 0);
     
-    motor(3, 0);
+    motor(right, 0);
     
-    msleep(1000);
+    msleep(500);
     
     
     //backwards
     
-    motor(0, -100);
+    motor(left, -50);
    
-    motor(3, -100);
+    motor(right, -50);
     
-    msleep(150);
+    msleep(350);
     
     
     //close claw
    
-    set_servo_position(3,2047);
+    set_servo_position(claw,2047);
     
     msleep(1000);
+    
+    
+    
+    //open claw
+    
+    set_servo_position(3,1000);
+    
+    msleep(1100);
+    
+    
+    
+    //turn
+    
+    motor(left,0);
+    
+    motor(right,50);
+    
+    msleep(515);
+    
+    
+    
+    //stop
+    
+     motor(left, 0);
+    
+     motor(right, 0);
+    
+     msleep(250);
+    
+    //opem arm
+  
+    set_servo_position(claw,1330);
+    msleep(1000);
+    
+    
+     
+    //raise arn
+    
+    set_servo_position(arm,1070);
+    
+    msleep(1000);
+    
+    
+    
+     //close claw
+   
+    set_servo_position(claw,2047);
+    
+    msleep(1000);
+    
+    
+    
+   //turn
+    
+     motor(left,105);
+    
+     motor(right,0);
+    
+     msleep(1000);
+ 
+    
+    
+    //forward
+    
+    motor(left,100);
+    
+    motor(right,100);
+    
+    msleep(500);
     
     
     //raise arn
     
-    set_servo_position(0,1085);
+    set_servo_position(arm,1800);
+    
+    msleep(1500);
+    
+    
+    
+    //turn
+    
+     motor(left,0);
+    
+     motor(right,95);
+    
+     msleep(1000);
+    
+    
+    
+    //backwards
+    
+    motor(left, -50);
+   
+    motor(right, -50);
+    
+    msleep(1750);
+   
+    
+    
+    //forward
+    
+	motor(left,5);
+    
+    motor(right,5);
+    
+    msleep(635);
+    
+    
+     //turn
+    
+     motor(left,0);
+    
+     motor(right,30);
+    
+     msleep(1340);
+    
+    //forward
+    
+    motor(left,30);
+   
+    motor(right,30);
     
     msleep(1000);
+    
+    
+    
  
     
     
     
+ 
     
+    																	
     return 0;
 }
