d7c3e4c39a12661e338bbd9a45b7302f10218c02
botball345 <102452108+botball345@users.noreply.github.com>
2024-04-05 12:07:02 -0700

    Add files via upload

---
+++ Roomba/Purple noodle mission.c
@@ -0,0 +1,13 @@
+#include "Roomba missions.h"
+#include <kipr/wombat.h>
+
+void purple_noodle() {
+    create3_connect();
+    
+    
+    
+    create3_drive_straight(1,0.5);
+    
+    create3_wait();
+    
+}
