fc992cb1d12837f9bbec2989ef0ab18920b927e9
botball345 <102452108+botball345@users.noreply.github.com>
2024-04-13 11:57:19 -0700

    I added the pom pom's mission

---
+++ wombat/main.c
@@ -0,0 +1,17 @@
+#include <kipr/wombat.h>
+#include "missions.h"
+#include "Robot.h"
+
+int main()
+{
+    printf("Hello World\n");
+    
+    mission_one();
+    
+    mission_two();
+    
+ 	mission_three();
+    
+    																	
+    return 0;
+}
---
+++ wombat/missions.h
@@ -0,0 +1,5 @@
+void mission_one();
+
+void mission_two();
+
+void mission_three();
