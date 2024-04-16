88970ef311331b840e8a7831767226b38bdc5a32
botball345 <102452108+botball345@users.noreply.github.com>
2024-04-16 13:41:47 -0700

    Made a simpler code for purple noodle mission.

--- Roomba/Purple noodle mission.c
+++ Roomba/Purple noodle mission.c
@@ -2,12 +2,47 @@
 #include <kipr/wombat.h>
 
 void purple_noodle() {
+    
     create3_connect();
     
+    enable_servos();
+        
+    //arm up/starting position
+	//set_servo_position(3,460);
     
+    //create3_wait();
     
-    create3_drive_straight(1,0.5);
+    //forward
+    
+    create3_drive_straight(0.75,1);
     
     create3_wait();
+    
+    //turns towards light switch
+    create3_rotate_degrees(-30,2);
+    
+    create3_wait();
+    
+    
+    
+    
+    
+    
+    
+    //goes forward 
+    //create3_drive_straight (0.2,0.2);
+    
+    //create3_wait();
+    
+    //turn back around to the purple noodle
+    
+    //create3_rotate_degrees(-175,5);
+    
+   // create3_wait();
+    
+   //goes towards purple noodle ready to grab
+    //create3_drive_straight(0.2,1);
+    
+    //create3_wait();
     
 }
